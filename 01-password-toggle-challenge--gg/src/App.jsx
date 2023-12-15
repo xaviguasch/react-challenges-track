28b574eb6f363ba75ae2253c27a4ea73ddcf2996
Xavi Guasch <xaviguasch@gmail.com>
2023-12-15 13:58:21 +0100

    added 01-password-toggle challenge

---
+++ 01-password-toggle-challenge--gg/src/App.jsx
@@ -0,0 +1,13 @@
+import LimitedTextInput from './components/LimitedTextInput'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <LimitedTextInput />
+    </>
+  )
+}
+
+export default App
