be6e4f9d3eb87909ca6f1110c20c80b029673e42
Xavi Guasch <xaviguasch@gmail.com>
2023-12-16 13:03:23 +0100

    added solution to 01-password-toggle challenge

--- 01-password-toggle-challenge--gg/src/components/LimitedTextInput.jsx
+++ 01-password-toggle-challenge--gg/src/components/LimitedTextInput.jsx
@@ -1,24 +1,34 @@
+import { useState } from 'react'
+
 import './LimitedTextInput.css'
 
 export default function LimitedTextInput({ characterLimit = 20 }) {
-  const inputValue = ''
+  const [inputValue, setInputValue] = useState('')
 
-  const handleChange = (e) => {}
+  const charsLeft = characterLimit - inputValue.length
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value)
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    // if input length is too long
-    // alert "The input exceeds the character limit. Please shorten your text."
-    // else
-    // alert "Thanks for your submission"
+    if (charsLeft < 0) {
+      alert('The input exceeds the character limit. Please shorten your text.')
+    } else {
+      alert('Thanks for your submission')
+      setInputValue('')
+    }
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor='limited-text-input'>Limited Text Input:</label>
-        <span className='no-error'>Characters remaining: TODO</span>
+        <span className={`${charsLeft >= 0 ? 'no-error' : 'error'}`}>
+          Characters remaining: {charsLeft}
+        </span>
       </div>
       <input
         type='text'
